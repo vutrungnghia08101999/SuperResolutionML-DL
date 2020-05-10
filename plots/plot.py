28fc0fedc166be5b7437021966ab98299fcc0a48
levuloihust99 <loi.lv173240@sis.hust.edu.vn>
2020-05-10 22:51:03 +0700

    add plots for Loss

--- plots/plot.py
+++ plots/plot.py
@@ -3,7 +3,60 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 path = '../charts/'
-def plot(param): # one of ['depth', 'BN', 'kernel_size', 'ReLU_Tanh']
+colors = ['b', 'r', 'm', 'c']
+x = np.arange(50) + 1
+
+def plot_graph(index, param, values, mapper):
+    """
+    :param index: one of ['PSNR', 'SSIM', 'Loss', 'Val_loss']
+    :param param: one of ['depth', 'BN', 'kernel_size', 'ReLU_Tanh']
+    :param values: values of legend in the graph
+    :param mapper: a dictionary in the format: index[legend] = array of values,
+            for example: PSNR['with BN'] = array of 50 float values corresponding to 50 epochs.
+    :return:
+    """
+    assert index in ['PSNR', 'SSIM', ['Loss', 'Val_loss']]
+    assert param in ['depth', 'BN', 'kernel_size', 'ReLU_Tanh']
+
+    if index in ['PSNR', 'SSIM']:
+        fig = plt.figure(figsize=(12, 5))
+        chart = fig.add_subplot()
+        chart.set_xlabel("Epochs")
+        chart.set_ylabel(index)
+        idx = 0
+
+        for i in values:
+            if param == 'kernel_size':
+                label = r"{0}$\times${1}".format(i, i)
+            elif param == 'depth':
+                label = r"depth = {}".format(i)
+            else:
+                label = i
+            chart.plot(x, mapper[i], color=colors[idx], label=label)
+            idx += 1
+
+        chart.legend()
+        fig.savefig("{0}{1}_{2}.pdf".format(path, param, index))
+    else:
+        loss = mapper["Loss"]
+        val_loss = mapper["Val_loss"]
+        idx = 0
+        for i in values:
+            fig = plt.figure(figsize=(12, 5))
+            chart = fig.add_subplot()
+            chart.set_xlabel("Epochs")
+            chart.set_ylabel("Loss")
+            chart.plot(x, loss[i], color='b', label='Training loss')
+            chart.plot(x, val_loss[i], color='r', label='Validate loss')
+            chart.legend()
+            fig.savefig("{0}{1}_{2}_{3}.pdf".format(path, index[idx], param, i))
+
+def plot(param):
+    """
+    one of ['depth', 'BN', 'kernel_size', 'ReLU_Tanh']
+    :param param: one of ['depth', 'BN', 'kernel_size', 'ReLU_Tanh']
+    :return:
+    """
     if param == 'depth':
         values = [2, 4, 6, 8]
     elif param == 'kernel_size':
@@ -38,48 +91,13 @@ def plot(param): # one of ['depth', 'BN', 'kernel_size', 'ReLU_Tanh']
         val_loss[i] = np.array([x["Val_loss"] for x in data[idx]])
         idx += 1
 
-    x = np.arange(50) + 1
-
-    colors = ['b', 'r', 'm', 'c']
-
-    psnr = plt.figure(figsize=(12, 5))
-    psnr_chart = psnr.add_subplot()
-    psnr_chart.set_xlabel("Epochs")
-    psnr_chart.set_ylabel("PSNR")
-    idx = 0
-
-    for i in values:
-        if param == 'kernel_size':
-            label = r"{0}$\times${1}".format(i, i)
-        elif param == 'depth':
-            label = 'depth = {}'.format(i)
+    indices = ['PSNR', 'SSIM', ['Loss', 'Val_loss']]
+    for index in indices:
+        if index in ['PSNR', 'SSIM']:
+            plot_graph(index, param, values, mapper=PSNR if index == 'PSNR' else SSIM)
         else:
-            label = i
-        plt.plot(x, PSNR[i], color=colors[idx], label=label)
-        idx += 1
+            plot_graph(index, param, values, mapper={'Loss': loss, 'Val_loss': val_loss})
 
-    psnr_chart.legend()
-    psnr.savefig("{0}{1}_PSNR.pdf".format(path, param))
-
-    psnr = plt.figure(figsize=(12, 5))
-    psnr_chart = psnr.add_subplot()
-    psnr_chart.set_xlabel("Epochs")
-    psnr_chart.set_ylabel("SSIM")
-    idx = 0
-
-    for i in values:
-        if param == 'kernel_size':
-            label = r"{0}$\times${1}".format(i, i)
-        elif param == 'depth':
-            label = 'depth = {}'.format(i)
-        else:
-            label = i
-        plt.plot(x, SSIM[i], color=colors[idx], label=label)
-        idx += 1
-
-    psnr_chart.legend()
-    psnr.savefig("{0}{1}_SSIM.pdf".format(path, param))
-    
 params = ['depth', 'BN', 'kernel_size', 'ReLU_Tanh']
 for param in params:
     plot(param)
