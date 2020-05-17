77617f8c2e1360e660bfaf40456832642a91221b
vutrungnghia <nghia.vt173284@sis.hust.edu.vn>
2020-05-17 08:34:14 +0700

    LUSHEETA-done-test-BIBUBIC

--- utils.py
+++ utils.py
@@ -1,17 +1,33 @@
-import torch
+import math
 import numpy as np
-from skimage.metrics import structural_similarity as ssim
+from PIL import Image
 
-def calc_psnr(img1, img2):
-    # print(type(img1))
-    # print(img1.shape)
-    # print(img1)
-    return 10. * torch.log10(1. / torch.mean((img1 - img2) ** 2))
+from skimage.metrics import structural_similarity
+from torchvision.transforms import ToTensor
 
-def calc_ssim(img1, img2):
-    img1 = np.array(img1.squeeze())
-    img2 = np.array(img2.squeeze())
-    return ssim(im1=img1, im2=img2, data_range=1)
+def convert_to_y_channel(img: Image.Image):
+    """Conver and RGB image to y channel, the output in range (0, 255)
+    """
+    img = np.array(img.convert('RGB'))
+    return 16. + (64.738 * img[..., 0] + 129.057 * img[..., 1] + 25.064 * img[..., 2]) / 256
+
+
+def calculate_psnr_y_channel(img1: Image.Image, img2: Image.Image) -> float:
+    """
+    Arguments:
+        img1 {Image.Image} -- RGB image
+        img2 {Ima
+    """
+    y_img1 = convert_to_y_channel(img1)
+    y_img2 = convert_to_y_channel(img2)
+    return 10 * math.log10(1.0/((y_img1/255.0 - y_img2/255.0)**2).mean() + 1e-8)  # makesure y_img1 and y_img2 in range(0, 1)
+
+
+def calculate_ssim_y_channel(img1: Image.Image, img2: Image.Image):
+    y_img1 = convert_to_y_channel(img1)
+    y_img2 = convert_to_y_channel(img2)
+    return structural_similarity(y_img1 / 255.0, y_img2 / 255.0)  # makesure y_img1 and y_img2 in range(0, 1)
+
 
 class AverageMeter(object):
     def __init__(self):
